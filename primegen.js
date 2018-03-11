cbf5b309c79bcf0a89cc5c43981e4cce68b1610a
Anton Osika <anton.osika@gmail.com>
2018-03-11 21:11:18 +0100

    Add parsing of format and writing of format, with recursive repetiton of subelements.

--- primegen.js
+++ primegen.js
@@ -30,7 +30,7 @@ osc.type = 'square';
 let osc2 = audioCtx.createOscillator();
 osc2.type = 'square';
 
-let ground = 310;
+let ground = 300;
 
 let base = [2.0, 3.0];
 console.log(base)
@@ -39,7 +39,6 @@ let maxIntervalChange = 3
 let maxToneCoeff = 4
 let maxAccordChange = 2;
 
-let t = 0
 let tones = [];
 for (let i = -maxIntervalChange ; i < maxIntervalChange + 1; i++) {
     for (let j = -maxIntervalChange ; j < maxIntervalChange + 1; j++) {
@@ -47,8 +46,6 @@ for (let i = -maxIntervalChange ; i < maxIntervalChange + 1; i++) {
         let tone = [i, j];
         let f = freq(base, tone);
         if (f > 0.5 && f < 2.0 ) {
-            // osc.frequency.setValueAtTime(ground * f, audioCtx.currentTime + t/2);
-            // t += 1;
             console.log(tone, f);
             tones.push(tone);
         }
@@ -83,23 +80,152 @@ function newTone(tone, maxIntervalChange) {
     return [t, f]
 }
 
+
 let tone = [0, 0];
-let length = Math.floor(Math.random()*20);
-length = 10
-for (let i = 0 ; i < length ; i++) {
+song = [[]]
+
+let t = 0
+for (let j = 0 ; j < 3 ; j++){
+  let length = t + 2**Math.floor(Math.random());
+  length=5
+  block = []
+
+  while (t < length) {
     let [t1, f1] = newTone(tone, maxIntervalChange)
     let [t2, f2] = newTone(t1, maxAccordChange)
+
     tone = t1
     console.log(t1, t2, f1, f2)
-    osc.frequency.setValueAtTime(ground * f1, audioCtx.currentTime + t/2);
-    osc2.frequency.setValueAtTime(ground * f2, audioCtx.currentTime + t/2);
-    let diff = (Math.floor(Math.random()*2) + 1)/2;
+    osc.frequency.setValueAtTime(ground * f1, audioCtx.currentTime + t);
+    osc2.frequency.setValueAtTime(ground * f2, audioCtx.currentTime + t);
+
+    let diff = 0.5**Math.floor(Math.random()*2)/2
+    if (diff + t > length) {
+      diff = length - t
+    }
     t += diff;
+
+    block.push([[t1, t2], diff])
+  }
+  song[0].push(block)
 }
 
+for (let level = 1 ; level < 2 ; level++) {
+song.push([])
+  // for (let i = 0 ; i < 3 ; i++){
+    let block = []
+    let length = Math.floor(Math.random()*3) + 1
+    length = 10
+    for (let j = 0 ; j < length ; j++){
+      let idx = Math.floor(Math.random()*song[level-1].length)
+      block.push(idx)
+    }
+    song[level].push(block)
+  // }
+}
+
+
+let fl = false
+if (fl) {
 osc.connect(audioCtx.destination);
 osc.start();
-osc.stop(audioCtx.currentTime + t/2);
+osc.stop(audioCtx.currentTime + t);
 osc2.connect(audioCtx.destination);
 osc2.start();
-osc2.stop(audioCtx.currentTime + t/2);
+osc2.stop(audioCtx.currentTime + t);
+}
+
+document.getElementById('input').value = JSON.stringify(song)
+
+class Player {
+  constructor() {
+    this.audioCtx = new AudioContext()
+    this.masterGain = this.audioCtx.createGain()
+    this.masterGain.connect(this.audioCtx.destination)
+    this.oscs = []
+  }
+
+  initOscillators() {
+    // Clear old oscillators
+    this.oscs.map(osc => {
+      osc.stop()
+      osc.disconnect()
+    })
+
+    // Set up oscillators, 10 should be enough
+    const numOscs = 10
+    this.oscs = []
+    for (let i = 0; i < numOscs; i++) {
+      const osc = this.audioCtx.createOscillator()
+      osc.connect(this.masterGain)
+      osc.type = 'square';
+      osc.frequency.setValueAtTime(0, 0)
+      osc.start()
+      this.oscs.push(osc)
+    }
+  }
+
+  flatten(song, start) {
+    const above = song.slice(0, song.length-1)
+    const level = song[song.length-1]
+    const block = level[start]
+
+    if (song.length == 1) {
+      return block
+    }
+
+    let res = []
+
+    for (let i = 0; i < block.length; i++) {
+      const ref = block[i]
+      const flat_block = this.flatten(above, ref)
+      res = res.concat(flat_block)
+    }
+    return res
+  }
+
+  play(song) {
+    this.initOscillators()
+
+    const baseFreq = 300
+    const toneLength = 0.5 // seconds
+    let t = this.audioCtx.currentTime
+
+    let block = this.flatten(song, 0)
+    block.push([[], 0]) // End with silence
+    console.log(block)
+
+    for (let i = 0; i < block.length; i++) {
+      const tones = block[i][0]
+
+      for (let j = 0; j < this.oscs.length; j++) {
+        const osc = this.oscs[j]
+
+        // Only activate as many oscillators as there are specified ratios
+        let f = 0
+        if (j < tones.length) {
+          f = freq(base, tones[j])
+        }
+
+        osc.frequency.setValueAtTime(baseFreq * f, t)
+        // Avoid clipping:
+        this.masterGain.gain.setValueAtTime(2.0 / Math.max(1, tones.length), t)
+      }
+      t += block[i][1]
+    }
+  }
+}
+
+const player = new Player()
+
+document.addEventListener('keydown', (event) => {
+  if (!(event.key === 'Enter' && event.ctrlKey)) return
+  // Delay execution until next event loop so that DOM has time to update
+  setTimeout(() => {
+    const song = document.getElementById('input').value
+    player.play(JSON.parse(song))
+  }, 0)
+})
+
+
+
